f75366a711fdbbed5a7efff68b38d01cd9aa1f9f
Adelais <lilingfengzero@gmail.com>
2020-05-17 10:01:34 +0800

    Refactor optioanl version.
    Change-Id: I46ec0c49e93c65661a97ebbc0d52978d09ee0a19

--- RxSemaphore/RxSemaphore.swift
+++ RxSemaphore/RxSemaphore.swift
@@ -11,15 +11,6 @@ import RxSwift
 public enum RxSemaphoreAction<Value> {
     case wait(Value)
     case signal
-
-    public var value: Value? {
-        switch self {
-        case .wait(let value):
-            return value
-        case .signal:
-            return nil
-        }
-    }
 }
 
 public typealias RxSemaphoreTransform<Element> = (Element) -> RxSemaphoreAction<Element>
@@ -76,54 +67,47 @@ extension ObservableType {
             return semaphore(collection) { .wait($0) }
     }
 
+}
+
+public protocol OptionalProtocol {
+    var isNil: Bool { get }
+    static var createNil: Self { get }
+}
+
+extension Optional: OptionalProtocol {
+
+    public var isNil: Bool {
+        switch self {
+        case .some:
+            return false
+        case .none:
+            return true
+        }
+    }
+
+    public static var createNil: Optional<Wrapped> {
+        return nil
+    }
+}
+
+extension ObservableType where Element: OptionalProtocol {
+
     public static func semaphore<C: Swift.Collection>(_ collection: C)
-        -> Observable<Element?> where C.Element == Observable<Element?> {
-            let seed: BehaviorSubject<RxSemaphoreAction<Element>> = BehaviorSubject(value: .signal)
-            let sources = collection.map { o in
-                return o.materialize()
-                    .flatMap({ event -> Observable<RxSemaphoreAction<Element>> in
-                        switch event {
-                        case .next(let maybeElement):
-                            if let element = maybeElement {
-                                return .just(.wait(element))
-                            } else {
-                                return .just(.signal)
-                            }
-                        case .error(let error):
-                            return .error(error)
-                        case .completed:
-                            return .just(.signal)
-                        }
-                    })
-                    .map { value -> BehaviorSubject<RxSemaphoreAction<Element>> in
-                        let subject = BehaviorSubject(value: value)
-                        if case .signal = value {
-                            subject.onCompleted()
-                        }
-                        return subject
-                }
-                .scan(seed) { (previous, next) in
-                    previous.onCompleted()
-                    return next
-                }
-                .map {
-                    $0.asObservable()
+        -> Observable<Element> where C.Element == Observable<Element> {
+            return semaphore(collection) { element -> RxSemaphoreAction<Element> in
+                if element.isNil {
+                    return .signal
+                } else {
+                    return .wait(element)
                 }
             }
-            return Observable.merge(sources)
-                .concatMap { o -> Observable<RxSemaphoreAction<Element>> in
-                    return o.concat(Observable.just(.signal))
-            }
-            .distinctUntilChanged { (l, r) -> Bool in
-                switch (l, r) {
-                case (.signal, .signal):
-                    return true
-                default:
-                    return false
+            .map { value -> Element in
+                switch value {
+                case .wait(let element):
+                    return element
+                case .signal:
+                    return .createNil
                 }
-            }
-            .map {
-                $0.value
             }
     }
 
